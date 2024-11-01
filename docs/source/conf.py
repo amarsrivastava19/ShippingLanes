c84779d9359c31537e2f2dbcc7acf8f141f0f422
amarsrivastava19 <49100740+amarsrivastava19@users.noreply.github.com>
2024-10-31 19:11:38 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ShippingLanes'
+copyright = '2024, Srivastava'
+author = 'Srivastava'
 
 release = '0.1'
 version = '0.1.0'
